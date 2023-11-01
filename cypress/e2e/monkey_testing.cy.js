22fb7d31af6db0cdc40e32b8cbdfd6a6ecba48d7
Irina Sinning <i.sinning@uniandes.edu.co>
2023-10-31 21:00:56 -0500

    Se realizan ajustes al codigo para que realice los pasos solicitados, se crea archivo readme y los requerimientos solicitados

--- cypress/e2e/monkey_testing.cy.js
+++ cypress/e2e/monkey_testing.cy.js
@@ -3,8 +3,8 @@ describe('Los estudiantes under monkeys', function() {
         cy.visit('https://losestudiantes.co');
         cy.wait(1000);
         randomEvent(10);
-    })
-})
+    });
+});
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -21,47 +21,76 @@ function randomEvent(monkeysLeft) {
     ];
 
     if(monkeysLeft > 0) {
-        var randomEvent = events[getRandomInt(0, events.length)];
-        randomEvent();
-        cy.wait(1000);
-        randomEvent(monkeysLeft - 1);
+        var chosenEvent = events[getRandomInt(0, events.length)];
+        chosenEvent().then((success) => {
+            if(success) {
+                cy.wait(1000);
+                randomEvent(monkeysLeft - 1);
+            } else {
+                randomEvent(monkeysLeft - 1);
+            }
+        });
     }
 }
 
 function clickRandomLink() {
-    cy.get('a').then($links => {
+    return cy.get('a').then($links => {
         var randomLink = $links.get(getRandomInt(0, $links.length));
         if(!Cypress.dom.isHidden(randomLink)) {
             cy.wrap(randomLink).click({force: true});
+            return cy.wrap(true);
         }
+        return cy.wrap(false);
     });
 }
 
 function fillRandomTextField() {
-    cy.get('input[type="text"]', { timeout: 10000 }).then($inputs => {
-        var randomInput = $inputs.get(getRandomInt(0, $inputs.length));
-        if(!Cypress.dom.isHidden(randomInput)) {
-            cy.wrap(randomInput).type('Random Text');
+    return cy.get('body').then($body => {
+        if ($body.find('input[type="text"]').length > 0) {
+            return cy.get('input[type="text"]').then($inputs => {
+                var randomInput = $inputs.get(getRandomInt(0, $inputs.length));
+                if (!Cypress.dom.isHidden(randomInput) && !randomInput.disabled && !randomInput.readOnly) {
+                    cy.wrap(randomInput).type('Random Text', { force: true });
+                    return cy.wrap(true);
+                } 
+                return cy.wrap(false);
+            });
+        } else {
+            return cy.wrap(false);
         }
     });
 }
 
 function selectRandomCombo() {
-    cy.get('select', { timeout: 10000 }).then($combos => {
-        var randomCombo = $combos.get(getRandomInt(0, $combos.length));
-        if(!Cypress.dom.isHidden(randomCombo)) {
-            var optionsLength = randomCombo.children.length;
-            var randomOptionIndex = getRandomInt(0, optionsLength);
-            cy.wrap(randomCombo).select(randomCombo.children[randomOptionIndex].value);
+    return cy.get('body').then($body => {
+        if ($body.find('select').length > 0) {
+            return cy.get('select').then($combos => {
+                var randomCombo = $combos.get(getRandomInt(0, $combos.length));
+                if(!Cypress.dom.isHidden(randomCombo)) {
+                    var options = randomCombo.children;
+                    var optionsLength = options.length;
+                    if (optionsLength > 0) {
+                        var randomOptionIndex = getRandomInt(0, optionsLength);
+                        var optionValue = options.item(randomOptionIndex).value;
+                        cy.wrap(randomCombo).select(optionValue);
+                        return cy.wrap(true);
+                    }
+                }
+                return cy.wrap(false);
+            });
+        } else {
+            return cy.wrap(false);
         }
     });
 }
 
 function clickRandomButton() {
-    cy.get('button').then($buttons => {
+    return cy.get('button').then($buttons => {
         var randomButton = $buttons.get(getRandomInt(0, $buttons.length));
         if(!Cypress.dom.isHidden(randomButton)) {
             cy.wrap(randomButton).click({force: true});
+            return cy.wrap(true);
         }
+        return cy.wrap(false);
     });
 }
